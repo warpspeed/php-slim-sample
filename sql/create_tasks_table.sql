96928d35af43dd6001ec782799107efdbde1a360
samcallahan <scallahan18@amherst.edu>
2017-04-20 16:27:28 -0400

    Added support for new mySQL modes.

--- sql/create_tasks_table.sql
+++ sql/create_tasks_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS tasks (
 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-	`created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-	`updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+	`created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
 	`is_complete` tinyint(1) NOT NULL DEFAULT '0',
 	PRIMARY KEY (`id`))
